1c0ee69065d3e087484f7ab27153235df4c22b2a
KanekiiCat <alex123viri@gmail.com>
2025-06-09 13:53:45 +0200

    feat(Extra): Crear clase abstracta Extra

---
+++ src/main/java/edu/estatuas/galleygrub/extras/Extra.java
@@ -0,0 +1,25 @@
+package edu.estatuas.galleygrub.extras;
+
+import java.util.Optional;
+
+import edu.estatuas.galleygrub.order.Comanda;
+
+public abstract class Extra {
+
+    final static String CHEESE = "";
+    final static String SAUCE = "";
+    final static String SIZE_LARGE = "";
+    String extraProduct = "";
+    Optional<Extra> nextExtra;
+
+    public Extra() {
+    }
+
+    public void setExtraProduct(Extra extra) {
+        this.extraProduct = extraProduct;
+    }
+
+    public abstract void sumExtra(Comanda comanda);  
+
+
+}
