4393e5dc1d33c2cfe5bd4c7036998b5de8033b93
KanekiiCat <alex123viri@gmail.com>
2025-06-09 13:52:19 +0200

    feat(Recipt): Crear clase Recipt

---
+++ src/main/java/edu/estatuas/galleygrub/receipt/Receipt.java
@@ -0,0 +1,49 @@
+package edu.estatuas.galleygrub.receipt;
+
+import edu.estatuas.galleygrub.extras.Extra;
+import edu.estatuas.galleygrub.order.Comanda;
+
+public class Receipt implements Ticket {
+    
+    private Double total = 0.0;
+    private Comanda order;
+    private Extra firstExtra;
+
+    public Receipt(Comanda comanda) {
+    }
+
+    @Override
+    public Comanda getOrder() {
+        return this.order;
+    }
+
+    @Override
+    public void setChain(Extra extra) {
+        this.firstExtra = extra;
+    }
+
+    @Override
+    public Extra getChain() {
+        return this.firstExtra;
+    }
+    
+
+    @Override
+    public void sumExtraCharge() {
+
+    }
+
+    @Override
+    public Double total() {
+        return this.total;
+    }
+
+    @Override
+    public void print() {
+    }
+
+    
+    
+
+
+}
